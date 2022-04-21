a8dcbad572b91cd629d36066e20ecefb8f17e71c
aleksandar angelevski <stotka123@yahoo.com>
2022-04-21 20:32:06 +0200

    set-loading-work-on-alert

--- src/components/Todoitems.js
+++ src/components/Todoitems.js
@@ -1,18 +1,26 @@
 import React from "react";
 
-function Todoitems({ todo }) {
+function Todoitems({ deleteTodos, todo }) {
   return (
-    <div className="todo-container">
+    <div className="container">
       <section className="section-center">
-        <main key={todo.id}>
-          {todo.map((item) => {
-            const { id, title, completed } = item;
-            return (
-              <div className="content">
-                <h2>{title}</h2>
-              </div>
-            );
-          })}
+        <main className="row">
+          <div className="col s12 m6">
+            {todo.map((item) => {
+              const { id, title, completed } = item;
+              return (
+                <div className="card blue-grey" key={id}>
+                  <div className="card-content white-text">
+                    <span className="card-title">{title}</span>
+                    <span>{completed}</span>
+                  </div>
+                  <button onClick={() => deleteTodos(id)} className="btn">
+                    delete
+                  </button>
+                </div>
+              );
+            })}
+          </div>
         </main>
       </section>
     </div>
--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Todoitems from "../components/Todoitems";
+import Alert from "../components/alert/Alert";
+import Loading from "../components/Loading";
+import DropDown from "../components/DropDown";
 const url = "https://jsonplaceholder.typicode.com/todos";
+const usersUrl = "https://jsonplaceholder.typicode.com/users";
+
+///////////////////////////////////////////////
 function TodoList() {
   const [todo, setTodos] = useState([]);
-  /////////// feth Todos ////////////
+  const [users, setUsers] = useState([]);
+  const [completedTodo, setCompletedTodo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  /////////// fetch Todos ////////////
+
+  const deleteTodos = (id) => {
+    const removeTodos = todo.filter((item) => item.id !== id);
+
+    setTodos(removeTodos);
+  };
+
+  //////
 
   const fetchTodos = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    setTodos(data);
-
+    setLoading(true);
     try {
+      const response = await fetch(url);
+
+      const data = await response.json();
+
+      const cutArray = data.slice(1, 10);
+
+      console.log(data);
+
+      setTodos(cutArray);
+
+      const res = await fetch(usersUrl);
+      const dataUsers = await res.json();
+      console.log("data users", dataUsers);
+      setLoading(false);
     } catch (err) {
       console.log(err);
     }
@@ -23,9 +50,19 @@ function TodoList() {
   }, []);
   /////// done ////////
 
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return (
     <>
-      <Todoitems todo={todo} />
+      {loading && <p className="loader"></p>}
+      <DropDown />
+      <Todoitems todo={todo} deleteTodos={deleteTodos} />
     </>
   );
 }
